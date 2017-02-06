42db16446b36d1df96ca54deda0624c18b927d0a
Alan Gutierrez <alan@prettyrobots.com>
2017-02-06 15:57:31 -0600

    Use the Makefile.

---
+++ bin/make.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+homeport module <<usage
+    usage: wg make <target>
+
+    description:
+
+        Create something form the Wiseguy Makefile.
+usage
+
+pushd "$wiseguy_path/../.." > /dev/null
+project_path=$(pwd)
+popd > /dev/null
+
+make -C "$project_path" -f "$wiseguy_path/Makefile" "$@"
